15ce449f3bb372a132a75e2b04407685ae59b972
bharat <bharatjoshi.p@gmail.com>
2018-06-04 18:05:14 +1000

    better colors for hl-line and selection in company and ivy/helm

--- lib/zeno-theme.el
+++ lib/zeno-theme.el
@@ -73,8 +73,8 @@
      (warning-bg-face        "#333333")
      (fullWhite              "#FFFFFF")
      (fullBlack              "#000000")
-     (hl-line-highlight      "#1F2029")
-     (line-highlight         "#000000")
+     (hl-line-highlight      "#343850")
+     (line-highlight         "#343850")
      (mode-line-active       "#2A3146")
      (mode-line-inactive     "#1d2130")
      
