e8ddc4a6f20614d72d5f3cae6e337b81372a67fd
bharat joshi <bharatjoshi.p@gmail.com>
2018-08-01 21:34:19 +1000

    disable unicode in modeline

--- init.el
+++ init.el
@@ -163,7 +163,7 @@ values."
    dotspacemacs-inactive-transparency 90
    dotspacemacs-show-transient-state-title t
    dotspacemacs-show-transient-state-color-guide t
-   dotspacemacs-mode-line-unicode-symbols t
+   dotspacemacs-mode-line-unicode-symbols nil
    dotspacemacs-smooth-scrolling t
    dotspacemacs-line-numbers nil
    dotspacemacs-folding-method 'evil
